7c349cf51af18bfc01e23a88505be2adaeb0d19d
Devi Prasad Reddy P <devi.reddy@kickdrumtech.com>
2026-02-27 22:43:08 +0530

    new commit

--- src/main/java/com/example/demo/VersionController.java
+++ src/main/java/com/example/demo/VersionController.java
@@ -14,4 +14,8 @@ public class VersionController {
     public String getVersion() {
         return "Application Version: " + version;
     }
+    @GetMapping("/")
+    public String getHi(){
+        return "Hi, from main branch";
+    }
 }
